93426e8655d8d7b89daae479e3890fe1ac4844e9
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-03-25 11:16:31 -0300

    coreção httsp busca tel

--- src/pages/Telefones.js
+++ src/pages/Telefones.js
@@ -17,7 +17,7 @@ function Telefones() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://cpf.lussandro.com.br/api/consulta_telefone?ddd=${ddd}&telefone=${telefone}`);
+      const response = await axios.get(`https://cpf.lussandro.com.br/api/consulta_telefone?ddd=${ddd}&telefone=${telefone}`);
       setTelefoneData(response.data.data);
       setError(null);
     } catch (error) {
